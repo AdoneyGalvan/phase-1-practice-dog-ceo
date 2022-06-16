abf7001b14c11063d2838afeece4a513c6b79692
Aron Galvan <arongalvan03@gmail.com>
2022-06-15 20:57:31 -0700

    commit tree be81515a28f4d06e9a20219dbbb87b576fcd7d34

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+        const dogImageContainer = document.getElementById("dog-image-container");
+        for (const dog of data.message)
+        {
+            const image = document.createElement("img");
+            image.src = dog;
+            dogImageContainer.append(image);
+        }
+    })
+    .catch(error => console.log(error));
+
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => {
+        let dogBreeds = document.getElementById("dog-breeds");
+        for (const dog of Object.keys(data.message))
+        {
+            const breed = document.createElement("li");
+            breed.innerText = dog;
+            dogBreeds.append(breed);
+        }
+        
+        document.querySelectorAll("li").forEach(dogBreed => {
+            dogBreed.addEventListener('click', (e) => {
+                e.target.style.color = 'red';
+            })
+        })
+
+        const breedDropDown = document.getElementById('breed-dropdown');
+        
+        breedDropDown.addEventListener('click', (e) => {
+            const dogs = Object.keys(data.message).filter(dog => dog[0] == e.target.value);
+            
+            dogBreeds.innerHTML = "";
+
+            for (const dog of dogs)
+            {
+                const breed = document.createElement("li");
+                breed.innerText = dog;
+                dogBreeds.append(breed);
+            }
+            
+            document.querySelectorAll("li").forEach(dogBreed => {
+                dogBreed.addEventListener('click', (e) => {
+                    e.target.style.color = 'red';
+                })
+            })
+        })
+
+    })
+    .catch(error => console.log(error));
+});
